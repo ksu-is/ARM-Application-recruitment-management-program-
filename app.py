450714b68124e50721d6508a665e941dc9b15d57
JessicaLi39 <jlinyard@students.kennesaw.edu>
2021-12-08 02:21:42 -0500

    More code

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import database 
 
+
 MENU_PROMPT = """
  Please choose from the following options:
 
@@ -7,7 +8,8 @@ MENU_PROMPT = """
  2) view all applicants.
  3) Find applicant by name
  4) Find applicants best for job.
- 5) Quit
+ 5) Update applicant 
+ 6) Quit
 
  Your selection:"""
 
@@ -17,27 +19,41 @@ def menu():
     print("Welcome to ARMS! Your Application Recruitment Management System!")
 
 
-    while (user_input := input(MENU_PROMPT)) != "5":
+    while (user_input := input(MENU_PROMPT)) != "6":
         if user_input == "1":
             first_name = input("Enter applicant  first name: ")
             last_name = input("Enter applicant last name: ")
-            phone_number = int(input("Enter applicant phone number: "))
+            phone_number = input("Enter applicant phone number: ")
             level = int(input("Enter applicant assessment level (1-5): "))
             age_range = int(input("Enter in the applicant age-range: "))
             interested_jobs = input("Enter in applicant interested jobs: ")
             availability = input("Enter applicant availability: ")
             location_miles = int(input("Enter location in miles: "))
+            print("New appliacant has been saved!")
 
             database.add_applicant(connection, first_name, last_name, phone_number, level, age_range, interested_jobs, availability, location_miles)
-
+             
         elif user_input == "2":
             applicants = database.get_all_applicants(connection)
             for applicant in applicants:
                 print(applicant)
         elif user_input =="3":
-            pass
+            first_name = input("Enter applicant name to find: ")
+            applicants = database.get_applicants_by_first_name(connection, first_name)
+            for applicant in applicants: 
+                print(applicant)
+            else:
+                print("Sorry, there is no applicant with that name!")    
+
         elif user_input =="4":
-            pass
+            interested_jobs = input("Enter the job your looking for")
+            interested_jobs = database.get_best_applicant_for_job(connection, interested_jobs)
+            print(f" The applicants interested in this job is: {interested_jobs[2]}")
+        elif user_input =="5":
+            update_applicants = input("Enter name of applicant you would like to update.")
+            applicants= database.update_applicants(connection, update_applicants)
+            print("update succesful")
+        
         else:
             print("Invalid input, please enter a valid option!")
 
--- database.py
+++ database.py
@@ -1,18 +1,26 @@
-import sqlite3 #importing sqlite3 to get my database 
+
+import sqlite3
+from sqlite3.dbapi2 import Connection #importing sqlite3 to get my database 
+
+
+
+
+
 
 #
-CREATE_APPLICANT_TABLE = "CREATE TABLE IF NOT EXISTS applicants (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, phone_number INTEGER, level INTEGER, age_range INTEGER, interested_jobs TEXT, availibility TEXT, location_miles INTEGER);"
+CREATE_APPLICANT_TABLE = "CREATE TABLE IF NOT EXISTS applicants (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, phone_number INTEGER, level INTEGER, age_range INTEGER, interested_jobs TEXT, availability TEXT, location_miles INTEGER);"
 INSERT_APPLICANT = "INSERT INTO applicants (first_name, last_name, phone_number, level, age_range, interested_jobs, availability, location_miles) VALUES (?, ?, ?, ?, ?, ?, ?, ?);"
 
 
 #queries to find an applicant by name 
 GET_ALL_APPLICANTS = "SELECT * FROM applicants;"
-GET_APPLICANTS_BY_NAME = "SELECT * FRROM applicants WHERE name = ?;"
+GET_APPLICANTS_BY_FIRST_NAME = "SELECT * FROM applicants WHERE first_name = ?;"
 GET_BEST_APPLICANT_FOR_JOB = """
 SELECT * FROM applicants
 WHERE interested_jobs = ?
 ORDER BY level DESC
-LIMIT 1;"""
+LIMIT 5;"""
+GET_APPLICANT_TO_EDIT = ""
 
 
 def connect():
@@ -35,12 +43,31 @@ def get_all_applicants(connection):
 
 
 # Writeing function for get applicant by name 
-def get_applicants_by_name(connection, name):
+def get_applicants_by_first_name(connection, name):
     with connection:
-        return connection.execute(GET_APPLICANTS_BY_NAME, (name,)).fetchall()
+        return connection.execute(GET_APPLICANTS_BY_FIRST_NAME, (name,)).fetchall()
 
 
 #function for finding the best applicant for the job
 def get_best_applicant_for_job(connection, interested_job):
     with connection:
         return connection.execute(GET_BEST_APPLICANT_FOR_JOB, (interested_job,)).fetchone()
+
+def update_applicants():
+    Conn = sqlite3.connect('applicants.db')
+    c = Conn.cursor()
+
+    c.execute("""UPDATE applicants SET
+        first_name = :first,
+        last_name = :last,
+        phone_number = :number,
+        interested_jobs = :jobs,
+        availability = :availability,
+        location_miles = :miles
+
+        WHERE oid = :oid""")
+
+
+
+
+
